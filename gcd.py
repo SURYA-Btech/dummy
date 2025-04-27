2c290681a7ad94ae8c5866fdcb3e65bd52a704e9
SURYA-Btech <sec23cb051@sairamtap.edu.in>
2025-04-27 12:28:25 +0530

    Update gcd.py

--- gcd.py
+++ gcd.py
@@ -4,7 +4,7 @@ c1, c2 = s.columns(2)
 with c1:
     s.write("hi Ms.")
     i=Image.open("p.png")
-    s.i(i,use_column_width=True)
+    s.image(i,use_column_width=True)
 with c2:
     s.header("priyadharshini\n goto side bar")
     s.sidebar.subheader("are you lucky? select any opiton in side bar")
@@ -15,7 +15,7 @@ if o == "1":
         n=s.text_input("enter your luck number")
         sub=s.form_submit_button("submit pannu")
         if sub:
-            st.success("u r lucky because surya is your prand")
+            s.success("u r lucky because surya is your prand")
     
 elif o == "2":
      with s.form("enter the details"):
